9ffab1bdfb9a2d34d15bb9e46f7391a36f92fe3b
andreagioelepiazza <140707822+andreagioelepiazza@users.noreply.github.com>
2023-08-17 15:52:04 +0200

    Update RecensioneController.java
    aggiunta commenti

--- src/main/java/com/Scuola/demo/controller/RecensioneController.java
+++ src/main/java/com/Scuola/demo/controller/RecensioneController.java
@@ -17,7 +17,8 @@ public class RecensioneController {
 	
 	@Autowired
 	RecensioneService sr;
-	
+
+	//Metodo che porta a una pagina in cui sono mostrati gli oggetti di tipo "Recensione"
 	@RequestMapping(value="/recensioni", method = RequestMethod.GET)
 	public String lista(Model model) {
 	
@@ -26,7 +27,9 @@ public class RecensioneController {
 		return "recensioni";
 		
 	}
-	
+
+	/*Metodo che prende i dati inseriti dall'utente per creare un nuovo oggetto di tipo "Recensione"
+	e reindirizza alla pagina in cui sono mostrati gli oggetti di tipo "Recensione" */
 	@RequestMapping(value="/recensione/create", method = RequestMethod.POST)
 	public String insDatiEffettuata(String nome, String titolo, String recensione) {
 		
